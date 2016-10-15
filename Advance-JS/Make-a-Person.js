1d0f45574e18eda787bdcf989267c7c436ebf4b5
Daniel <github@profoundideation.com>
2016-10-15 08:58:33 -0400

    mod 1
    mod 1

--- Advance-JS/Make-a-Person.js
+++ Advance-JS/Make-a-Person.js
@@ -1,50 +1,70 @@
 var Person = (function() {
   var firstName, lastName;
-  // , fullName = firstAndLast;  
   
-  function Person(firstAndLast) {    
-    firstName = firstAndLast.split(' ')[0];
-    lastName = firstAndLast.split(' ')[1]; 
-  
-    // this.setFullName(firstAndLast);    
-
-    // this.setFullName = function(name) {fullName = names;};
+  function Person(firstAndLast) {      
+    var fullName = firstAndLast, nameArr = firstAndLast.split(' ');     
+     /* 
+       var name = firstAndLast.split(' ');
+       firstName = name[0];
+       lastName = name[1];     
+     */
+    /*
+      firstName = firstAndLast.split(' ')[0];
+      lastName = firstAndLast.split(' ')[1]; 
+    */
+    
+    firstName = nameArr[0];
+    lastName = nameArr[1];
+    
+    /*
+    console.log(firstName);
+    console.log(lastName);
+    */
+    
+    // console.log(nameArr[0]);
+    // return nameArr[0];
   }
+  
+  Person.prototype.setFirstName = function(firstN) {
+    this.first = firstN;
+  }; 
+
+  Person.prototype.setLastName = function(lastN) {
+    this.last = lastN;
+  };
 
   Person.prototype.getFirstName = function() {
-    return this.first;
+    return firstName;
   };
-
+  
   Person.prototype.getLastName = function() {
-    return this.last;
+    return lastName;
   };
-
+  
   Person.prototype.getFullName = function() {
-    return this.first + ' ' + this.last;
+    return firstName + ' ' + lastName;
   };
-
-  Person.prototype.setFirstName = function(first) {
-    this.first = first;
-  };
-
-  Person.prototype.setLastName = function(last) {
-    this.last = last;
-  };
-
+    
   Person.prototype.setFullName = function(firstAndLast) {
-    var names = firstAndLast.split(' ');
-    this.first = names[0];
-    this.last = names[1];
+    // var names = firstAndLast.split(' ');
+    /*  */ 
+    firstName = firstAndLast.split(' ')[0];
+    lastName = firstAndLast.split(' ')[1];  
+       
+    // return firstName + ' ' + lastName;
   };
+    
 
   return Person;
-  })();
+  // return nameArr[0];
+})();
 
 var bob = new Person('Bob Ross');
 
 // Object.keys(bob).length;
-console.log(Object.keys(bob).length); // should return 6.
-bob.getFullName();  // Bob Ross
+// console.log("bob: ", Object.keys(bob).length); // should return 6.
+// console.log("bob: ", bob);
+bob.setFirstName();  // Bob Ross
 
 // console.log("bob: ", bob);
 
