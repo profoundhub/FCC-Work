5bc454dc8a4da8e950d6c15b63510e68ccf8daf2
Daniel <github@profoundideation.com>
2016-10-15 08:09:57 -0400

    re-try
    re-try

--- Advance-JS/Make-a-Person.js
+++ Advance-JS/Make-a-Person.js
@@ -1,35 +1,45 @@
 var Person = function(firstAndLast) {
-  this.setFullName(firstAndLast);
+  var fullName = firstAndLast;  
+  
+  this.setFullName(firstAndLast);    
+
+  // this.setFullName = function(name) {fullName = names;};
 };
 
-Person.prototype.getFirstName = function() {
-  return this.first;
-};
+  Person.prototype.getFirstName = function() {
+    return this.first;
+  };
 
-Person.prototype.getLastName = function() {
-  return this.last;
-};
+  Person.prototype.getLastName = function() {
+    return this.last;
+  };
 
-Person.prototype.getFullName = function() {
-  return this.first + ' ' + this.last;
-};
+  Person.prototype.getFullName = function() {
+    return this.first + ' ' + this.last;
+  };
 
-Person.prototype.setFirstName = function(first) {
-  this.first = first;
-};
+  Person.prototype.setFirstName = function(first) {
+    this.first = first;
+  };
 
-Person.prototype.setLastName = function(last) {
-  this.last = last;
-};
+  Person.prototype.setLastName = function(last) {
+    this.last = last;
+  };
 
-Person.prototype.setFullName = function(firstAndLast) {
-  names = firstAndLast.split(' ');
-  this.first = names[0];
-  this.last = names[1];
-};
+  Person.prototype.setFullName = function(firstAndLast) {
+    names = firstAndLast.split(' ');
+    this.first = names[0];
+    this.last = names[1];
+  };
+
 
 var bob = new Person('Bob Ross');
 
+// Object.keys(bob).length;
+console.log(Object.keys(bob).length); // should return 6.
 bob.getFullName();  // Bob Ross
-console.log(bob instanceof Person);  // true
+
+// console.log("bob: ", bob);
+
+// console.log(bob instanceof Person);  // true
 // Object.keys(bob).length;
