dcb52c5af749b99707d5f5edd9798a858b299eae
Daniel <github@profoundideation.com>
2016-10-16 17:22:32 -0400

    why
    why

--- Advance-JS/Make-a-Person.js
+++ Advance-JS/Make-a-Person.js
@@ -1,111 +1,37 @@
 var Person = (function() {
   var firstName, lastName;
   
-  function Person(firstAndLast) {      
-    var fullName = firstAndLast /* , nameArr = firstAndLast.split(' ') */;     
-     
-       var name = firstAndLast.split(' ');
-       firstName = name[0];
-       lastName = name[1];     
-     
-    /* 
+  function Person(firstAndLast) {
     
-      firstName = firstAndLast.split(' ')[0];
-      lastName = firstAndLast.split(' ')[1]; 
-    */
-    /*
-    firstName = name[0];
-    lastName = name[1];
-    
-    */
-    /*
-    console.log(firstName);
-    console.log(lastName);
-    */
-    
-    // console.log(nameArr[0]);
-    // return nameArr[0];
+    var fullName = firstAndLast;
 
+    this.getFirstName = function() {
+      return fullName.split(' ')[0];
+    };
 
+    this.getLastName = function() {
+      return fullName.split(' ')[1];
+    };
+
+    this.getFullName = function() {
+      return fullName;
+    };
+
+    this.setFirstName = function(name) {
+      fullName = name + " " + fullName.split(' ')[1];
+    };
+
+    this.setLastName = function(name) {
+      fullName = fullName.split(' ')[0] + " " + name;
+    };
+
+    this.setFullName = function(name) {
+      fullName = name;
+    };
     
-  this.getFirstName = function() {
-    return firstName;
-  };
+  }
   
-  this.getLastName = function() {
-    return lastName;
-  };
-    
+var bob = new Person("Bob Ross");
+bob.getFullName();
   
-  this.setFirstName = function(nameF) {
-    this.first = nameF;
-    fullName = name + ' ' + fullName.split(' ')[1];
-    return fullName;
-  };
-/*    
-  this.setFirstName = function(first) {
-    this.first = first;
-    nameArr[0] = first;    
-  }; 
-*/
-    
-  this.setLastName = function(nameL) {
-    this.last = nameL;
-    fullName = firstAndLast.split(' ')[0] + ' ' + name;
-    return fullName;
-  };
-    
-  this.getFullName = function() {
-    return firstName + ' ' + lastName;
-    // fullName = name;
-  };
-      
-  this.setFullName  = function(firstAndLast) {
-    fullName = nameFull;
-    /*nameArr = firstAndLast.split(' '); */
-  };
-}
-  /*
-  Person.prototype.getFirstName = function() {
-    return firstName;
-  };
-  
-  Person.prototype.getLastName = function() {
-    return lastName;
-  };
-  
-  Person.prototype.getFullName = function() {
-    return firstName + ' ' + lastName;
-  };
-    */
-  
-  Person.prototype.setFullName = function(firstAndLast) {
-    // var names = firstAndLast.split(' ');
-    /* */ 
-    /* */
-      firstName = firstAndLast.split(' ')[0];
-      lastName = firstAndLast.split(' ')[1];  
-    
-       // nameArr = firstAndLast.split(' '); 
-    // return firstName + ' ' + lastName;
-  };
-    
-  return Person;
-  // return nameArr[0];
 })();
-
-var bob = new Person('Bob Ross');
-
-bob.setFirstName("Haskell");
-bob.setLastName("Curry");
-bob.getFirstName();
-// Object.keys(bob).length;
-// console.log("bob: ", Object.keys(bob).length); // should return 6.
-// console.log("bob: ", bob);
-
-// bob.setFirstName();  // Bob Ross
-
-// console.log("bob: ", bob);
-
-// console.log(bob instanceof Person);  // true
-// Object.keys(bob).length;
