386bf6d95748c3601e3807c063a0d0009287aecf
Daniel Lim <hbt@markhamboards.com>
2016-10-25 17:59:51 -0400

    Update Caesars Cipher
    Update Caesars Cipher

---
+++ Algorithms/Caesars-Cipher.js
@@ -0,0 +1,18 @@
+function rot13(str) { // LBH QVQ VG!
+
+  var newString = [];
+
+  for (var i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) < 65 || str.charCodeAt(i) > 90) {
+      newString.push(str.charAt(i));
+    } else if (str.charCodeAt(i) > 77) {
+      newString.push(String.fromCharCode(str.charCodeAt(i) - 13));
+    } else {
+      newString.push(String.fromCharCode(str.charCodeAt(i) + 13));
+    }
+  }
+  return newString.join("");
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
