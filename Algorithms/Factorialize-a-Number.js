4db080f71b348ad375ae398c1da443476219e009
Daniel Lim <hbt@markhamboards.com>
2016-11-06 16:12:41 -0500

    factorialize es6
    factorialize + es6

--- Algorithms/Factorialize-a-Number.js
+++ Algorithms/Factorialize-a-Number.js
@@ -1,3 +1,14 @@
+let factorialize = (num) => {
+ let factorial = 1;
+  for (let n = 2; n <= num; n++) {
+    factorial = factorial * n;
+  }
+  return factorial;
+};
+factorialize(5);
+
+/*
+
 function factorialize(num) {
  var factorial = 1;
   for (let n = 2; n <= num; n++) {
@@ -6,3 +17,5 @@ function factorialize(num) {
   return factorial;
 }
 factorialize(5);
+
+*/
