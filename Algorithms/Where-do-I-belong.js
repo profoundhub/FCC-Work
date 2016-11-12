d549f4f3584ef10dcf19d3918c1c85a723523264
Daniel Lim <hbt@markhamboards.com>
2016-11-12 13:54:50 -0500

    where-do-i-belong Solution
    where-do-i-belong Solution

--- Algorithms/Where-do-I-belong.js
+++ Algorithms/Where-do-I-belong.js
@@ -1,25 +1,10 @@
-// Where do I belong ?
-
-function getIndexToIns(arr, num) {
-  // Find my place in this sorted array.
-
-  // Push Num into Array
-
+let getIndexToIns = (arr, num) => {
   arr.push(num);
-
-  // Sort Array
-
   arr.sort(function(a, b) {
     return a - b;
     }
   );
-
-  // get Index of Num.
-
   return arr.indexOf(num);
-
-  // console.log(arr);
-  
-}
+};
 
 // readme: Algorithms\Where-do-I-belong.md
