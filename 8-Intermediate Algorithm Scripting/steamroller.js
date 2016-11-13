b2645f5b1efd22133d77655ce54e0b159c3d8f31
Daniel <github@profoundideation.com>
2016-11-13 00:20:04 -0500

    steamroller.js ES6
    steamroller.js ES6

--- 8-Intermediate Algorithm Scripting/steamroller.js
+++ 8-Intermediate Algorithm Scripting/steamroller.js
@@ -1,58 +1,17 @@
-
-var myArray = [];
-
-function steamrollArray(arr) {
-  // I'm a steamroller, baby
-  
-  // Let's get rid of all other arrays ...
-  firstArray = [];
-  flatArr(arr);
-  
-  function flatArr(arr) {
-    arr.forEach(function(arr2nd ) {
-    
+"use strict";
+let myArray = [];
+let steamrollArray = (arr) => {
+  let firstArray = [];    
+  flatArr(arr);  
+  let flatArr = (arr) => {
+    arr.forEach(function(arr2nd) {
     if (!Array.isArray(arr2nd )) {
-      firstArray.push(arr2nd);      
-      
+      firstArray.push(arr2nd);     
     } else {
        flatArr(arr2nd);
-  
-      
     }   
   });
-}
-  
-  
+};
   return firstArray; // myArray;
-  
-}  
-  
-  steamrollArray([1, [2], [3, [[4]]]]);
-  
-  
-
-/*  
-
-  // I'm a steamroller, baby
-  
-  // Let's get rid of all other arrays ...
-
-
-  for (var i = 0; i<arr.length; i++) {
-    
-    if (!Array.isArray(arr[i])) {      
-      myArray.push(arr[i]);      
-      
-    } else {
-      
-      steamrollArray(arr[i]);      
-    }
-  }  
-  return myArray;
-}
-
+};
 steamrollArray([1, [2], [3, [[4]]]]);
-
-*/
-// steamrollArray([[["a"]], [["b"]]]);
-// steamrollArray([1, {}, [3, [[4]]]]);
