e11575c834ed84e3063b89cd7b63b01ef8c8595f
Daniel Lim <hbt@markhamboards.com>
2016-11-13 19:15:29 -0500

    update Seek and Destroy
    update Seek and Destroy

--- 6-Basic-Algorithm-Scripting/seek-and-destroy.js
+++ 6-Basic-Algorithm-Scripting/seek-and-destroy.js
@@ -1,8 +1,10 @@
+// Seek and Destroy
 let destroyer = (arr) => {
-  let args = Array.prototype.slice.call(arguments);
-  args.splice(0, 1);
+  let arg = Array.prototype.slice.call(arguments);
+  arg.splice(0,1);
   return arr.filter(function(element) {
-    return args.indexOf(element) === -1;
+    return arg.indexOf(element) === -1;
   });
 };
+
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
