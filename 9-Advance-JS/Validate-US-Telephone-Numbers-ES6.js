0e1dac4026438be75e2b39008813c16818e26d4f
Daniel Lim <hbt@markhamboards.com>
2016-11-14 10:15:17 -0500

    Validate US Telephone Numbers + ES6
    Validate US Telephone Numbers + ES6

---
+++ 9-Advance-JS/Validate-US-Telephone-Numbers-ES6.js
@@ -0,0 +1,7 @@
+/* Validate US Telephone Numbers */
+
+let telephoneCheck = (str) => {
+  let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    return regex.test(str);  
+};
+telephoneCheck("555-555-5555");
