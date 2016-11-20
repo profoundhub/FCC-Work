aea52ad7327fac50865501dca24e7d94ef4ecc5f
Daniel Lim <hbt@markhamboards.com>
2016-11-20 12:41:38 -0500

    init add Falsy Bouncer
    Falsy Bouncer

---
+++ 6-Basic-Algorithm-Scripting/falsy-bouncer.js
@@ -0,0 +1,22 @@
+function bouncer(arr) {
+  // Don't show a false ID to this bouncer.
+  
+  var filteredArray = arr.filter(Boolean);
+  return filteredArray;
+  
+//  var iArray;
+  
+//  function filterArray(obj) {
+//  if (arr === null || arr === "0" || arr === undefined || arr === "NaN" ) {
+//    return true;
+//  } else {
+//    iArray++;
+//    return false;
+//  }
+//}
+  
+//  var arrModify = arr.filter(filterArray);
+//  return arr;
+}
+
+bouncer([7, "ate", "", false, 9]);
