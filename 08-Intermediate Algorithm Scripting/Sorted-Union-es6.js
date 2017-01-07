ce7deab5189ea66084d850dba98741dce18dcc80
Daniel <github@profoundideation.com>
2017-01-07 00:08:59 -0500

    Update Sorted-Union-es6.js
    Update Sorted-Union-es6.js

--- 08-Intermediate Algorithm Scripting/Sorted-Union-es6.js
+++ 08-Intermediate Algorithm Scripting/Sorted-Union-es6.js
@@ -1,10 +1,10 @@
-function uniteUnique(arr1, arr2, arr3) {  
+const uniteUnique = (arr1, arr2, arr3) => {  
   var specialArray = [];  
   
-  for (var i=0; i<arguments.length; i++) {
+  for (let i=0; i<arguments.length; i++) {
     var arrArg = arguments[i];        
   
-  for (var j=0; j<arrArg.length; j++) {
+  for (let j=0; j<arrArg.length; j++) {
     var indexVal = arguments[i][j]; 
     
     if (specialArray.indexOf(arguments[i][j]) ===-1) {      
@@ -13,5 +13,5 @@ function uniteUnique(arr1, arr2, arr3) {
   }
 }  
   return specialArray;
-}
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]); // for ()
+};
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
