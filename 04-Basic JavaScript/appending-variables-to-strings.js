4fc21f801634063e5b5b1c6bfe1c917732b4f39c
Daniel <github@profoundideation.com>
2017-09-10 17:40:36 -0400

    + appending-variables-to-strings.js
    ++ appending-variables-to-strings.js

---
+++ 04-Basic JavaScript/appending-variables-to-strings.js
@@ -0,0 +1,13 @@
+# Appending Variables to Strings
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "Free Code Camp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "fucking awesome!";
+var myStr = "Learning to code is ";
+
+myStr += someAdjective;
