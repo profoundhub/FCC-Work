48fda6a8540bd9cc0587f2f9048e89e9d88dfcd1
Daniel <github@profoundideation.com>
2017-09-10 17:44:11 -0400

    + appending-variables-to-strings-es6.js
    ++ appending-variables-to-strings-es6.js

---
+++ 04-Basic JavaScript/appending-variables-to-strings-es6.js
@@ -0,0 +1,11 @@
+// Example
+let anAdjective = "awesome!";
+let ourStr = "Free Code Camp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+let someAdjective = "very awesome!";
+let myStr = "Learning to code is ";
+
+myStr += someAdjective;
