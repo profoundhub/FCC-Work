b5b931fddc1de23bbbf128083c41620a5ccc93e9
Daniel <github@profoundideation.com>
2017-09-10 17:47:38 -0400

    Update Find-the-Length-of-a-String.js

--- 04-Basic JavaScript/Find-the-Length-of-a-String.js
+++ 04-Basic JavaScript/Find-the-Length-of-a-String.js
@@ -1,3 +1,16 @@
 # Find the Length of a String 
 # Find-the-Length-of-a-String.js
 
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName;
