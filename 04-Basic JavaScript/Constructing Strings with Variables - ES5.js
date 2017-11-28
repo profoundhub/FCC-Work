84529d07689c3979c8da1df25666ab13f5edc2d2
Daniel <github@profoundideation.com>
2017-11-28 14:46:26 -0500

    Update Constructing Strings with Variables - ES5.js

--- 04-Basic JavaScript/Constructing Strings with Variables - ES5.js
+++ 04-Basic JavaScript/Constructing Strings with Variables - ES5.js
@@ -1,1 +1,9 @@
 // Constructing Strings with Variables - ES5.js
+
+// Example
+var ourName = "Free Code Camp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Daniel Lim";
+var myStr = "My name is " + myName + " and I am swell!";
