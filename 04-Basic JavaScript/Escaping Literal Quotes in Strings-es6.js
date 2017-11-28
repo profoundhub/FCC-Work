3f11938026acf1cd6383a7379cfe05a2878ca83e
Daniel <github@profoundideation.com>
2017-11-28 14:29:17 -0500

    Starting ... with ES6
    Starting ... with ES6

--- 04-Basic JavaScript/Escaping Literal Quotes in Strings-es6.js
+++ 04-Basic JavaScript/Escaping Literal Quotes in Strings-es6.js
@@ -1,4 +1,6 @@
-
+// starting ... with es5
 var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
 
+// ES6
+let myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
 
