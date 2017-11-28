4f31e9b519d79f9c7787bdb0a3079c8b6f78f2ce
Daniel <github@profoundideation.com>
2017-11-28 14:51:05 -0500

    Update Constructing Strings with Variables - ES6.js
    ES6 - using "Template Strings"
    
    BUT, this code (above) does not work since ... the test requirement includes this test: 
    
    "Use two + operators to build myStr with myName inside it"

--- 04-Basic JavaScript/Constructing Strings with Variables - ES6.js
+++ 04-Basic JavaScript/Constructing Strings with Variables - ES6.js
@@ -7,3 +7,10 @@ let ourStr = "Hello, our name is " + ourName + ", how are you?";
 // Only change code below this line
 let myName = "Daniel Lim";
 let myStr = "My name is " + myName + " and I am swell!";
+
+// ES6 - using "Template Strings"
+let myStr = `My name is ${myName} and I am swell!`;
+
+// BUT - This code (above) does not work since ... 
+// the test is looking for : 
+// "Use two + operators to build myStr with myName inside it"
