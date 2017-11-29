4e4e8750b25a5bfaa80a87f7b153e45f8f125f53
Daniel Lim <hbt@markhamboards.com>
2017-11-29 14:04:16 -0500

    + sol for C 6

---
+++ 04-Basic JavaScript/6-Understanding Case Sensitivity in Variables-es6.js
@@ -0,0 +1,10 @@
+
+// Declarations
+var studlyCapVar = 10;
+var properCamelCase  = "A String";
+var titleCaseOver  = 9000;
+
+// Assignments
+studlyCapVar  = 10;
+properCamelCase  = "A String";
+titleCaseOver  = 9000;
