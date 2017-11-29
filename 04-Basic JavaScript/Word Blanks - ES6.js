91de3df66fd7462a4ac8555271f893dad36c24d2
Daniel <github@profoundideation.com>
2017-11-28 21:02:03 -0500

    Update Word Blanks - ES6.js

--- 04-Basic JavaScript/Word Blanks - ES6.js
+++ 04-Basic JavaScript/Word Blanks - ES6.js
@@ -1,1 +1,13 @@
 // Word Blanks - ES6
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  let result = "";
+  // Your code below this line
+  result = "The " + myNoun + " was " + myAdjective + ", but " + myVerb + " very " + myAdverb + " .";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
