c147c5ce46f3aaf1e121a520fe93fc68f6604202
Daniel Lim <hbt@markhamboards.com>
2017-11-29 13:55:17 -0500

    update other Solutions

--- 04-Basic JavaScript/4-Initializing Variables with the Assignment Operator - ES6.js
+++ 04-Basic JavaScript/4-Initializing Variables with the Assignment Operator - ES6.js
@@ -6,3 +6,9 @@ let ourVar = 19;
 // let a = 9; // Error: Initialize a to a value of 9 ... because we used let, instead of var
 
 var a = 9; // Have to use this to pass the test!
+
+// Tried:
+//
+// const a = 9;
+//
+// Failed!
