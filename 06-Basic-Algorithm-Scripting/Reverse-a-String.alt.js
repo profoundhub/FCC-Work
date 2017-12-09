77fbfd688fdaeada4ae9ff6c833f6096e60daf21
Daniel <github@profoundideation.com>
2017-12-09 11:24:51 -0500

    var solutions
    var solutions

---
+++ 06-Basic-Algorithm-Scripting/Reverse-a-String.alt.js
@@ -0,0 +1,27 @@
+
+let reverseString = (str) => {
+
+
+  var arr = str.split('');
+  arr.reverse();
+  arr.join();
+
+  
+  return arr;
+
+};
+
+reverseString("hello");
+
+/*
+  function reverseString(str) {
+    return str.split('').reverse().join('');
+  }
+  reverseString("hello");
+*/
+/*
+One Line Answer:
+let reverseString=(str)=>{return str.split('').reverse().join('');};reverseString("hello");
+*/
+
+
