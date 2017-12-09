d4f63e80ab27325328df6a6aab4ec58d28f3fd13
Daniel <github@profoundideation.com>
2017-12-09 11:54:15 -0500

    sol edit sol
    sol edit sol

--- 06-Basic-Algorithm-Scripting/palindrome.alt1.js
+++ 06-Basic-Algorithm-Scripting/palindrome.alt1.js
@@ -1,19 +1,18 @@
 let palindrome = (str) => {
   
-    // String.replace()
-    // String.toLowerCase()
+  // String.replace()
+  // String.toLowerCase()
+
+  str = str.toLowerCase().replace(/[^a-zA-Z0-9]+/g,"");
+  return str === str.split("").reverse().join("");
+
+  let string = "0_0 (: /-\ :) 0-0";
+  // console.log("string");
+  string.toLowerCase().replace(/[^a-zA-Z0-9]+/g,"").split("").reverse().join("");
+  // console.log("string");
+  return string;
+};
   
-    str = str.toLowerCase().replace(/[^a-zA-Z0-9]+/g,"");
-    return str === str.split("").reverse().join("");
-  
-    let string = "0_0 (: /-\ :) 0-0";
-      console.log("string");
-    string.toLowerCase().replace(/[^a-zA-Z0-9]+/g,"").split("").reverse().join("");
-      console.log("string");
-    return string;
-  };
-  
-  // alt?
-  
-  // palindrome("0_0 (: /-\ :) 0-0  123");
-  palindrome("eye");
+// palindrome("0_0 (: /-\ :) 0-0  123");
+
+palindrome("eye");
