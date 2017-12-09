708f75e85830dda74c97c1d868b9e72c52764054
Daniel <github@profoundideation.com>
2017-12-09 12:10:49 -0500

    optimizing Solutions
    optimizing Solutions

--- 06-Basic-Algorithm-Scripting/palindrome.es6.js
+++ 06-Basic-Algorithm-Scripting/palindrome.es6.js
@@ -1,14 +1,8 @@
 let palindrome = (str) => {
   
-    // String.replace()
-    // String.toLowerCase()
-  
     str = str.toLowerCase().replace(/[^a-zA-Z0-9]+/g,"");
-    return str === str.split("").reverse().join("");  
+    return str === str.split("").reverse().join("");
+  
   };
   
-  // alt?
-  
-  // palindrome("0_0 (: /-\ :) 0-0  123");
-
   palindrome("eye");
