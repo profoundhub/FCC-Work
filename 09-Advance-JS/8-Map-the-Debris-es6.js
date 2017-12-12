d246667ea48f04ba3cd801c607385398e5d92163
Daniel <github@profoundideation.com>
2017-12-12 15:50:30 -0500

    forgot my semi c
    I am human

--- 09-Advance-JS/8-Map-the-Debris-es6.js
+++ 09-Advance-JS/8-Map-the-Debris-es6.js
@@ -3,7 +3,7 @@ function orbitalPeriod(arr) {
     let GM = 398600.4418,
     earthRadius = 6367.4447,
     a = 2 * Math.PI,
-    newArr = []
+    newArr = [];
     let getOrbPeriod = function(obj) {
         let c = Math.pow(earthRadius + obj.avgAlt, 3),
         b = Math.sqrt(c / GM),
@@ -19,6 +19,5 @@ function orbitalPeriod(arr) {
     
     return newArr;
 }
-
 
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
