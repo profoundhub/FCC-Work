d6203e808eb5f2a6bd2b931e4ceed58612751dfa
Daniel <github@profoundideation.com>
2017-12-12 12:46:41 -0500

    focus on Factorializing
    focus on Factorializing

---
+++ 06-Basic-Algorithm-Scripting/Factorialize-a-Number.while-loop.comments.js
@@ -0,0 +1,21 @@
+
+function factorialize(num) {
+  var factorial = 1;
+    for (let n = 2; n <= num; n++) {
+      factorial = factorial * n;
+    }
+    return factorial;
+  }
+factorialize(5);
+
+/*
+Pattern: 
+
+0! = 1
+1! = 1
+2! = 2 * 1
+3! = 3 * 2 * 1
+4! = 4 * 3 * 2 * 1
+5! = 5 * 4 * 3 * 2 * 1
+
+*/
