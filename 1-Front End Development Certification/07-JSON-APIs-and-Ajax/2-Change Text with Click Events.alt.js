699d9860789cf0992b331e03e344b7583a76dc53
Daniel Lim <hbt@markhamboards.com>
2017-12-16 16:09:39 -0500

    Mod Update js files

--- 1-Front End Development Certification/07-JSON-APIs-and-Ajax/2-Change Text with Click Events.alt.js
+++ 1-Front End Development Certification/07-JSON-APIs-and-Ajax/2-Change Text with Click Events.alt.js
@@ -1,1 +1,31 @@
 // Change Text with Click Events - alt
+
+
+<script>
+  $(document).ready(function() {
+    $("#getMessage").on("click", function(){
+      // Only change code below this line.
+      
+      // Only change code above this line.
+    });
+  });
+</script>
+
+
+<div class="container-fluid">
+  <div class = "row text-center">
+    <h2>Cat Photo Finder</h2>
+  </div>
+  <div class = "row text-center">
+    <div class = "col-xs-12 well message">
+      The message will go here
+    </div>
+  </div>
+  <div class = "row text-center">
+    <div class = "col-xs-12">
+      <button id = "getMessage" class = "btn btn-primary">
+        Get Message
+      </button>
+    </div>
+  </div>
+</div>
